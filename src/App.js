62ef4d86928092949feb5b0a6e9c0b31ea1a0193
alishahi <shahy8227@gmail.com>
2023-04-21 05:27:59 -0700

    Add search by first and last name and email

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
-import { useState } from 'react';
-import {Users} from './users';
+import { useState } from "react";
+import { Users } from "./users";
+import Table from "./Table";
 
 function App() {
   const [query, setQuery] = useState("");
 
+  const search = (data) =>{
+    return data.filter((item) => 
+    item.first_name.toLowerCase().includes(query) ||
+    item.last_name.toLowerCase().includes(query) ||
+    item.email.toLowerCase().includes(query)
+    );
+  }
 
   return (
     <div className="app">
-      <input type="text" placeholder="جستجو..." className="search" onChange={(e)=> setQuery(e.target.value)} />
-      <ul className="list">
-        {
-          Users.filter((user)=>  user.first_name.toLowerCase().includes(query)).map((user) => (
-            <li key={user.id} className="listItem">{user.first_name}</li>
-          ))
-        }
-      </ul>
+      <input
+        type="text"
+        placeholder="جستجو..."
+        className="search"
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <Table data={search(Users)}/>
     </div>
   );
 }
